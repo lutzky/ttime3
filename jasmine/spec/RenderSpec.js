1c74a2653c36197c8cac3d38ebaae19672701372
Ohad Lutzky <ohad@lutzky.net>
2018-08-01 23:08:01 +0100

    Render: layoutLayeredEvents now works, is tested
    Also use numLayers instead of ratio to avoid moving floats around.

--- jasmine/spec/RenderSpec.js
+++ jasmine/spec/RenderSpec.js
@@ -1,17 +1,47 @@
 describe('Render', function() {
   it('should correctly lay out layered events', function() {
-    let events = {
-      A: { day: 1, startMinute: 5, endMinute: 25 },
-      B: { day: 1, startMinute: 0, endMinute: 15 },
-      C: { day: 1, startMinute: 20, endMinute: 35 },
-    };
+    let events = [
+      { day: 1, startMinute: 5, endMinute: 25 },
+      { day: 1, startMinute: 0, endMinute: 15 },
+      { day: 1, startMinute: 20, endMinute: 35 },
+      { day: 2, startMinute: 5, endMinute: 25 },
+      { day: 2, startMinute: 0, endMinute: 15 },
+      { day: 2, startMinute: 10, endMinute: 35 },
+    ];
 
-    // TODO(lutzky): We should actually be testing something about the return
-    // value of layoutLayeredEvents.
-    layoutLayeredEvents([events.A, events.B, events.C]);
+    let result = layoutLayeredEvents(events);
 
-    console.info(events);
-
-    expect(events).toBe(null);
+    expect(result).toEqual([
+      {
+        event: events[0],
+        layer: 0,
+        numLayers: 2,
+      },
+      {
+        event: events[1],
+        layer: 1,
+        numLayers: 2,
+      },
+      {
+        event: events[2],
+        layer: 1,
+        numLayers: 2,
+      },
+      {
+        event: events[3],
+        layer: 0,
+        numLayers: 3,
+      },
+      {
+        event: events[4],
+        layer: 1,
+        numLayers: 3,
+      },
+      {
+        event: events[5],
+        layer: 2,
+        numLayers: 3,
+      },
+    ]);
   });
 });
--- render.js
+++ render.js
@@ -4,7 +4,7 @@
  * @typedef {Object} LayeredEvent
  * @property {Event} event - Actual event
  * @property {number} layer - Collision layer for event
- * @property {number} ratio - How much to scale this event by
+ * @property {number} numLayers - The total number of layers
  *
  * Explanation: Suppose you have events A, B, C, that collide like so (time
  * being horizontal):
@@ -19,15 +19,15 @@
  * [BBBBBB][CCCCCC]
  *    [AAAAAAA]
  *
- * In this case, the ratio for all events is 1/2, B and C are on layer 0, and A
- * is on layer 1. If C were to start a bit earlier, though, three layers
- * would be needed:
+ * In this case, the numLayers for all events is 2, B and C are on layer 0, and
+ * A is on layer 1. If C were to start a bit earlier, though, three layers would
+ * be needed:
  *
  * [BBBBBB]
  *      [CCCCCC]
  *    [AAAAAAA]
  *
- * In this case the ratio for all events is 1/3, and B, C, and A are on layers
+ * In this case the numLayers for all events is 3, and B, C, and A are on layers
  * 0, 1, and 2 respectively.
  *
  */
@@ -38,14 +38,14 @@
  * Shamelessly lifted from boazg at
  * https://github.com/lutzky/ttime/blob/master/lib/ttime/tcal/tcal.rb
  *
- * @param {Event[]} events - Events to layer
+ * @param {Array<Event>} events - Events to layer
  *
- * @returns {LayeredEvent[]}
+ * @returns {Array<LayeredEvent>}
  */
 function layoutLayeredEvents(events) {
-  // TODO(lutzky): This does not yet actually return anything useful, and
-  // modifies its input, when it shouldn't.
+  /** @type {Array<LayeredEvent>} */
   let result = [];
+
   let remaining = events.slice();
 
   while (remaining.length > 0) {
@@ -69,31 +69,34 @@ function layoutLayeredEvents(events) {
       remaining = remaining.filter(x => !selected.has(x));
     }
 
+    /** @type {Array<Array<<Event>>} */
     let layers = [];
 
     selected.forEach(function(s) {
-      // TODO(lutzky): Assigning s.layer here breaks the Event type :/
-      s.layer = null;
-
+      let assignedToLayer = false;
       layers.some(function(layer, i) {
         if (!eventsCollide(layer.concat([s]))) {
+          assignedToLayer = true;
           layer.push(s);
-          s.layer = i;
           return true;
         }
       });
 
-      if (s.layer == null) {
+      if (!assignedToLayer) {
         // No layer has been assigned yet, so all layers must collide with
         // s. Create a new one.
         layers.push([s]);
-        s.layer = layers.length - 1;
       }
     });
 
-    let ratio = 1.0 / layers.length;
-    selected.forEach(function(s) {
-      s.ratio = ratio;
+    layers.forEach(function(l, i) {
+      l.forEach(function(s) {
+        result.push({
+          event: s,
+          layer: i,
+          numLayers: layers.length,
+        });
+      });
     });
   }
 
