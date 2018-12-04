1181422f97c062b42b6ebe488ab3c9f7693fd8b4
Ohad Lutzky <ohad@lutzky.net>
2018-12-04 18:26:19 +0000

    Webpack: Support --env.production

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/main.ts',
-  output: {
-    filename: 'ttime.js',
-    library: 'ttime',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/'
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js'],
-    modules: ['node_modules']
-  },
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by
-      // `ts-loader`
-      {test: /\.tsx?$/, loader: 'ts-loader'}
-    ]
+module.exports = env => {
+  isProduction = false;
+  if (env && env.production) {
+    isProduction = true;
   }
+  console.log('Production:', isProduction);
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'inline-source-map',
+    entry: './src/main.ts',
+    output: {
+      filename: 'ttime.js',
+      library: 'ttime',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: 'dist/'
+    },
+    resolve: {
+      // Add `.ts` and `.tsx` as a resolvable extension.
+      extensions: ['.ts', '.tsx', '.js'],
+      modules: ['node_modules']
+    },
+    module: {
+      rules: [
+        // all files with a `.ts` or `.tsx` extension will be handled by
+        // `ts-loader`
+        {test: /\.tsx?$/, loader: 'ts-loader'}
+      ]
+    }
+  };
 };
