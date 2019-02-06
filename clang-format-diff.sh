edf7bd44c003b2731d541a2ceef5216ac9e4e4a7
Ohad Lutzky <ohad@lutzky.net>
2019-02-05 22:17:43 -0800

    clang-format-diff: Run correctly in termux

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -4,10 +4,17 @@ set -e
 
 cd "$(dirname $0)"
 
+if [[ $(uname -o) == "Android" ]]; then
+  # Workaround for termux
+  BINARY=/data/data/com.termux/files/usr/bin/clang-format
+else
+  BINARY=node_modules/.bin/clang-format
+fi
+
 if [[ $1 == "fix" ]]; then
-  node_modules/.bin/clang-format -i $(find src/ spec/ -name '*.ts')
+  $BINARY -i $(find src/ spec/ -name '*.ts')
 fi
 
 for file in $(find src/ spec/ -name '*.ts'); do
-	diff --label $file --label $file -u $file <(node_modules/.bin/clang-format $file)
+	diff --label $file --label $file -u $file <($BINARY $file)
 done
