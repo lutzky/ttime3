a783faa0564d5007d11d6aeb2e18ad76bd0e8771
Matan Peled <chaosite@gmail.com>
2019-07-03 13:35:35 +0000

    Whitespace fixes.

--- src/cheesefork.ts
+++ src/cheesefork.ts
@@ -193,8 +193,8 @@ export function parse(jsData: string): Catalog {
 export function catalogNameFromUrl(url: string): string {
   const raw = url.substr(url.lastIndexOf('_') + 1, 6);
   const semesterNames:
-      { [semester: string]:
-       string } = { '01': 'Winter', '02': 'Spring', '03': 'Summer' };
+      {[semester: string]:
+           string } = {'01': 'Winter', '02': 'Spring', '03': 'Summer'};
   var year = raw.slice(0, 4)
   var semester = raw.slice(4)
   if (semester === '01') year = year + '/' + String(Number(year) + 1).slice(2);
